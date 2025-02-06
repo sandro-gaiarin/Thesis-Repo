a0e60e58ac8e0dd4606dde56e94d4919dabcefb7
starishsky <117216668+starishsky@users.noreply.github.com>
2025-02-06 04:00:32 -0500

    WHY ARE THERE SO MANY REDS

--- Assets/Scripts/BillboardScript.cs
+++ Assets/Scripts/BillboardScript.cs
@@ -5,6 +5,6 @@ public class BillboardScript : MonoBehaviour
     // Start is called before the first frame update
     private void Update()
     {
-        transform.rotation =Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
+        transform.rotation =Quaternion.Euler(Camera.main.transform.rotation.eulerAngles.x, Camera.main.transform.rotation.eulerAngles.y, 0f);
     }
 }
