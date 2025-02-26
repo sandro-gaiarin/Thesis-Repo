cda8e93ceae675e74d6625ee409b09ec0bf89e2d
starishsky <117216668+starishsky@users.noreply.github.com>
2025-02-25 19:29:55 -0500

    HELP HELP HELP HELP HELP

--- Assets/Scripts/SwitchCam.cs
+++ Assets/Scripts/SwitchCam.cs
@@ -8,17 +8,31 @@ public class SwitchCam : MonoBehaviour
     public CinemachineVirtualCamera TurnCamera;
     public CinemachineVirtualCamera TopCamera;
 
+    public CinemachineVirtualCamera R1Camera;
+    public CinemachineVirtualCamera R2Camera;
+    public CinemachineVirtualCamera R3Camera;
+
+    public CinemachineVirtualCamera H1Camera;
+    public CinemachineVirtualCamera H2Camera;
+    public CinemachineVirtualCamera H3Camera;
+    public CinemachineVirtualCamera H4Camera;
+    public CinemachineVirtualCamera Store1Camera;
+    public CinemachineVirtualCamera Store2Camera;
+    public CinemachineVirtualCamera Store3Camera;
+    public CinemachineVirtualCamera WCCamera;
+
+
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            CameraManager.SwitchCamera(TurnCamera);
+            CameraManager.SwitchCamera(H1Camera);
         }
 
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            CameraManager.SwitchCamera(StraightCamera);
+            CameraManager.SwitchCamera(WCCamera);
         }
 
     }
