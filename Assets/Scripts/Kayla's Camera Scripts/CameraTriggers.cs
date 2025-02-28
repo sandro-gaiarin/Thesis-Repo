66b406b259c54f8add03db5fde9910c4ef1c6a50
starishsky <117216668+starishsky@users.noreply.github.com>
2025-02-27 21:54:18 -0500

    camera collider stuff

---
+++ Assets/Scripts/Kayla's Camera Scripts/CameraTriggers.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraTriggers : MonoBehaviour
+{
+    // [SerializeField] private CinemachineVirtualCamera [] _cameraSettings;
+    // [SerializeField] private int _whichCamera;
+    public CinemachineVirtualCamera R1Camera;
+    public CinemachineVirtualCamera R2Camera;
+    public CinemachineVirtualCamera R3Camera;
+
+    public CinemachineVirtualCamera H1Camera;
+    public CinemachineVirtualCamera H2Camera;
+    public CinemachineVirtualCamera H3Camera;
+    public CinemachineVirtualCamera H4Camera;
+    public CinemachineVirtualCamera Store1Camera;
+    public CinemachineVirtualCamera Store2Camera;
+    public CinemachineVirtualCamera Store3Camera;
+    public CinemachineVirtualCamera WCCamera;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("H1Trigger"))
+        {
+            CameraManager.SwitchCamera(H1Camera);
+        }
+        if (other.CompareTag("H2Trigger"))
+        {
+
+            Debug.Log("H2Trigger");
+            CameraManager.SwitchCamera(H2Camera);
+        }
+        if (other.CompareTag("H3Trigger"))
+        {
+            CameraManager.SwitchCamera(H3Camera);
+        }
+        if (other.CompareTag("H4Trigger"))
+        {
+            CameraManager.SwitchCamera(H4Camera);
+        }
+        if (other.CompareTag("Store1Trigger"))
+        {
+            CameraManager.SwitchCamera(Store1Camera);
+        }
+        if (other.CompareTag("Store2Trigger"))
+        {
+            CameraManager.SwitchCamera(Store2Camera);
+        }
+        if (other.CompareTag("Store3Trigger"))
+        {
+            CameraManager.SwitchCamera(Store3Camera);
+        }
+        if (other.CompareTag("R1Trigger"))
+        {
+            CameraManager.SwitchCamera(R1Camera);
+        }
+        if (other.CompareTag("R2Trigger"))
+        {
+            CameraManager.SwitchCamera(R2Camera);
+        }
+        if (other.CompareTag("R3Trigger"))
+        {
+            CameraManager.SwitchCamera(R3Camera);
+        }
+        if (other.CompareTag("WCTrigger"))
+        {
+            CameraManager.SwitchCamera(WCCamera);
+        }
+    }
+}
